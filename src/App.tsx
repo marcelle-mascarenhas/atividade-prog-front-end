0a9c6587081bdd1f1c3c33bf8f126352face8871
marcelle-mascarenhas <marcellebts@gmail.com>
2022-03-09 07:43:55 -0300

    submit

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,6 @@ function App(): JSX.Element {
     'Tweet 2',
     'Tweet 3',
     'Tweet 4',
-    'Tweet 5',
-    'Tweet 6',
-    'Tweet 7',
-    'Tweet 8',
-    'Tweet 9', 
-    'Tweet 10', 
   ])
 
   function createTweet (){
